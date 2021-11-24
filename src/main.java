ff8b9e07b2ccdb6d396c8f5facfc2fc267972ff2
RavidYael <ravid.yael.7@gmail.com>
2021-11-24 19:05:52 +0200

    1859

--- src/main.java
+++ src/main.java
@@ -9,7 +9,7 @@ import org.w3c.dom.ranges.Range;
 import javax.xml.bind.SchemaOutputResolver;
 import java.util.Random;
 
-// ch ch changes ravid adding shit to this project again and again
+// ch ch changes ravid adding shit to this project again and again and again
 public class main {
     //    public static void main(String[] args) {
 //
