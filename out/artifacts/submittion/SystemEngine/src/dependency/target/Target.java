38e8b25d1e3fc93e46fc4ee25784a397b9438964
RavidYael <ravid.yael.7@gmail.com>
2022-01-08 18:29:18 +0200

    Changes by oatar

--- out/artifacts/submittion/SystemEngine/src/dependency/target/Target.java
+++ out/artifacts/submittion/SystemEngine/src/dependency/target/Target.java
@@ -15,14 +15,16 @@ public class Target implements Serializable {
     public static enum TargetStatus { Frozen, Skipped, Waiting ,InProcess, Finished, Done}
     public static enum TaskResult {Success, Warning, Failure,Skipped}
 
-
     private String name;
     private Set<String> requiredFor;
     private Set<String> dependsOn;
     private String data;
     private DependencyLevel dependencyLevel;
-    private TargetStatus targetStatus = TargetStatus.Frozen;
-    private TaskResult taskResult = TaskResult.Skipped;
+    // private TargetStatus targetStatus = TargetStatus.Frozen;
+    // private TaskResult taskResult = TaskResult.Skipped;
+
+    private SimpleObjectProperty<TaskResult> taskResult =  new SimpleObjectProperty(TaskResult.Skipped); // what is the initial value???
+    private SimpleObjectProperty<TargetStatus> targetStatus = new SimpleObjectProperty<>(TargetStatus.Skipped); // what is the initial value??
 
 
     public Target(String name,String data) {
@@ -31,6 +33,8 @@ public class Target implements Serializable {
         this.data = data;
         requiredFor = new HashSet<>();
         dependsOn = new HashSet<>();
+
+
     }
 
     public Target (Target other){
@@ -48,7 +52,8 @@ public class Target implements Serializable {
             dependsOn.add(dep);
         }
 
-        this.setTargetStatus(other.targetStatus);
+        this.setTargetStatus(other.targetStatus.get());
+
         this.setDependencyLevel(other.dependencyLevel);
 
     }
@@ -76,14 +81,26 @@ public class Target implements Serializable {
     public void setDependencyLevel(DependencyLevel dependencyLevel) {this.dependencyLevel = dependencyLevel;}
 
     public TargetStatus getTargeStatus() {
+        return targetStatus.get();
+    }
+
+    public TargetStatus getTargetStatus() {
+        return targetStatus.get();
+    }
+
+    public SimpleObjectProperty<TargetStatus> targetStatusProperty() {
         return targetStatus;
     }
 
+
     public TaskResult getTaskResult() {
-        return taskResult;
+        return taskResult.get();
     }
 
 
+    public SimpleObjectProperty<TaskResult> taskResultProperty() {
+        return taskResult;
+    }
 
     public void setName(String name) {
         this.name = name;
@@ -100,13 +117,58 @@ public class Target implements Serializable {
     public void setData(String data) {
         this.data = data;
     }
+
+//    public void setTargetStatus(TargetStatus targetStatus) {
+//        this.targetStatus = targetStatus;
+//    }
+
+
     public void setTargetStatus(TargetStatus targetStatus) {
-        this.targetStatus = targetStatus;
+        this.targetStatus.set(targetStatus);
     }
 
+
     public void setTaskResult(TaskResult taskResult) {
-        this.taskResult = taskResult;
+        this.taskResult.set(taskResult);
     }
+
+
+
+//    public void setTaskResult(TaskResult taskResult) {
+//        this.taskResult = taskResult;
+//    }
+
+    public void addTargetToDependsOrNeededFor(String targetName, Dependency dependency){
+        if (dependency == Dependency.DependsOn)
+            dependsOn.add(targetName);
+        else
+            requiredFor.add(targetName);
+
+    }
+
+    public Target getDeepCopy(){
+        Target copyTarget = new Target(name,data);
+        for (String curTargetName : dependsOn){
+            copyTarget.addTargetToDependsOrNeededFor(curTargetName,Dependency.DependsOn);
+        }
+        for (String curTargetName : requiredFor){
+            copyTarget.addTargetToDependsOrNeededFor(curTargetName,Dependency.RequiredFor);
+        }
+        return copyTarget;
+    }
+
+    public void removeTargetFromDependsOrNeeded(String targetName, Dependency dependency){
+        if (dependency == Dependency.DependsOn){
+            dependsOn.remove(targetName);
+        }
+        else{
+            requiredFor.remove(targetName);
+        }
+    }
+
+
+
+
 
     @Override
     public String toString() {
